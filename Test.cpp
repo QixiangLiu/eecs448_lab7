51e00e9ff55a42d20b0533bed516a8959c1b0671
Qixiang Liu <q709l816@ku.edu>
2018-04-09 16:20:42 -0500

    Start EECS448 Lab 7

---
+++ Test.cpp
@@ -0,0 +1,30 @@
+#include "Test.h"
+
+Test::Test(){
+
+}
+
+Test::~Test(){
+
+
+}
+
+void Test::run(){
+
+  LinkedListOfInts mylist;
+/*
+  vector<int> myVector = mylist.toVector();
+  cout << myVector.empty()<<endl;
+  cout << "Test01 isEmpty() \n";
+  if(mylist.isEmpty()){
+    cout << "List is empty\n";
+    cout << "IF empty, what is size of list?: ";
+    cout << mylist.size();
+  }else{
+    cout << "List is not empty\n";
+    cout << "IF Not empty, what is size of list?: ";
+    cout << mylist.size();
+  }
+*/
+
+}
---
+++ Test.h
@@ -0,0 +1,20 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+#include <vector>
+#include "LinkedListOfInts.h"
+
+using namespace std;
+
+
+class Test{
+public:
+
+  Test();
+  ~Test();
+  void run();
+
+};
+
+#endif
